7041b13baf1626f6f59c36c833498f839c7109c4
Varad Gundap <varad615@gmail.com>
2022-07-07 21:59:12 +0530

    upcoming fix

--- pages/upcoming.js
+++ pages/upcoming.js
@@ -3,46 +3,84 @@ import { HiHome } from "react-icons/hi";
 import React, { useEffect, useRef, useState } from "react";
 
 export default function Upcoming({ upcomingevents }) {
-  console.log(upcomingevents.description);
+  const [timerDays, setTimerDays] = useState(0);
+  const [timerHours, setTimerHours] = useState(0);
+  const [timerMinutes, setTimerMinutes] = useState(0);
+  const [timerSeconds, setTimerSeconds] = useState(0);
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const eventDate = upcomingevents.date;
+    const countDownDate = new Date(eventDate).getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+  console.log(timerDays);
   return (
     <div>
-           <div className="bg-gradient-to-t from-[#eeaeca] to-[#94bbe9] h-screen">
-             <section>
-               <div className="ml-5">
-                 <Link href="/">
-                   <button className="bg-white mt-5 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                     <div>
-                       <HiHome />
-                     </div>
-                   </button>
-                 </Link>
-               </div>
-               <div className="justify-center flex text-6xl text-center text-black">
-                 <div className="mx-4 mt-[8%]">
-                   <div className="font-bold">{upcomingevents.title}</div>
-                   <br />
-                   <div className="text-2xl">{upcomingevents.description}</div>
-                 </div>
-               </div>
-               <hr className="font-bold mx-[20%] mt-[15px]" />
-               <div className=" justify-center flex mt-9 text-black">
-                 <div className="flex flex-wrap gap-4 text-5xl">
-                   {/* <div>{timerDays} :</div>
-                   <div>{timerHours} :</div>
-                   <div>{timerMinutes} :</div>
-                   <div>{timerSeconds}</div> */}
-                 </div>
-               </div>
-             </section>
-             <div className="justify-center flex mt-10">
-               <Link href="/events">
-                 <button className="bg-[#2E3192] text-white p-3 rounded-xl">
-                   Other Events
-                 </button>
-               </Link>
-             </div>
-           </div>
-         </div>
+      <div className="bg-gradient-to-t from-[#eeaeca] to-[#94bbe9] h-screen">
+        <section>
+          <div className="ml-5">
+            <Link href="/">
+              <button className="bg-white mt-5 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <div>
+                  <HiHome />
+                </div>
+              </button>
+            </Link>
+          </div>
+          <div className="justify-center flex text-6xl text-center text-black">
+            <div className="mx-4 mt-[8%]">
+              <div className="font-bold">{upcomingevents.title}</div>
+              <br />
+              <div className="text-2xl">{upcomingevents.description}</div>
+            </div>
+          </div>
+          <hr className="font-bold mx-[20%] mt-[15px]" />
+          <div className=" justify-center flex mt-9 text-black">
+            <div className="flex flex-wrap gap-4 text-5xl">
+              <div>{timerDays} :</div>
+              <div>{timerHours} :</div>
+              <div>{timerMinutes} :</div>
+              <div>{timerSeconds}</div>
+            </div>
+          </div>
+        </section>
+        <div className="justify-center flex mt-10">
+          <Link href="/events">
+            <button className="bg-[#2E3192] text-white p-3 rounded-xl">
+              Other Events
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
