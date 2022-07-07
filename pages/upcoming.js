d131d33a67af7169af6dec9ebe0fd18be2240515
Varad Gundap <varad615@gmail.com>
2022-07-07 13:25:41 +0530

    test

--- pages/upcoming.js
+++ pages/upcoming.js
@@ -1,3 +1,11 @@
-export default function Upcoming() {
+export default function Upcoming({ upcomingevents }) {
+  console.log(upcomingevents);
   return <div>This is the upcoming page</div>;
 }
+
+Upcoming.getInitialProps = async () => {
+  const res = await fetch("https://runners-clan.vercel.app/api/posts");
+  const { data } = await res.json();
+
+  return { notes: data };
+};
