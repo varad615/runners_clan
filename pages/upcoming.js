f808a7812c64763d39f8cf18787b5aef346c3a53
Varad Gundap <varad615@gmail.com>
2022-07-07 14:31:59 +0530

    new api

--- pages/upcoming.js
+++ pages/upcoming.js
@@ -4,7 +4,7 @@ export default function Upcoming({ upcomingevents }) {
 }
 
 Upcoming.getInitialProps = async () => {
-  const res = await fetch("https://runners-clan.vercel.app/api/posts");
+  const res = await fetch("https://runners-clan.vercel.app/api/upcoming");
   const { data } = await res.json();
 
   return { upcomingevents: data };
